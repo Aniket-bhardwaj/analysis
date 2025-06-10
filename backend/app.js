a3251463a0e714ec4caddafbd9da945ca9563c7e
R1ULTRAMAX <rudra20031013@gmail.com>
2025-06-10 16:01:49 +0530

    Mounted and called download route from app.js

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const hideRoutes = require('./routes/hide');
 const authRoutes = require('./routes/auth');
 const graphRoutes = require('./routes/graphRoutes');
 const tableRoutes = require('./routes/tables'); 
+const downloadRouter = require('./routes/download.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(uploadDir));  // Serve uploaded files
 
+
 // Mount routes
 app.use('/', uploadRoutes);
 app.use('/', listRoutes);
@@ -35,6 +37,7 @@ app.use('/', hideRoutes);
 app.use('/auth', authRoutes);
 app.use('/', graphRoutes); // /api/graph-data
 app.use('/', tableRoutes);
+app.use('/', downloadRouter);
 
 // Start server
 app.listen(PORT, () => {
