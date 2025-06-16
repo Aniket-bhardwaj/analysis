3951c71ad376bc433d47c132264af8450722f68d
ShivaR004 <shivarath2000@gmail.com>
2025-06-16 16:11:41 +0530

    added a route to call qc_checks

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -5,8 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/login';
 import DashboardPage from './pages/homepage';
 import DataManagerPage from './pages/data_manager';
-import QualityCheck from './pages/qc_graphs';
-import QCTable from './pages/qc_tables';
+// import QualityCheck from './pages/qc_graphs';
+// import QCTable from './pages/qc_tables';
+import QCChecks from './pages/qc_checks';
 
 
 const AppRoutes = () => {
@@ -16,7 +17,7 @@ const AppRoutes = () => {
         <Route path="/" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/data-manager" element={<DataManagerPage />} />
-        <Route path= "/qc-checks" element={<QCTable />} />
+        <Route path= "/qc-checks" element={<QCChecks />} />
       </Routes>
     </Router>
   );
