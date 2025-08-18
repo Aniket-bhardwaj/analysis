e55edc3a543b2f0783757d81ca82a198fef8b946
jayanshj05 <jayanshj23@iitk.ac.in>
2025-08-18 18:42:24 +0530

    fixed a bug

--- backend/services/miniTableService.js
+++ backend/services/miniTableService.js
@@ -4,6 +4,7 @@ const fileModel = require("../models/fileModel");
 const miniTableService = {
   async getMiniTableForElement(fileId, solutionLabel, element) {
     try {
+      console.log("Fetching mini table for element:", element, "in fileId:", fileId, "with solutionLabel:", solutionLabel);
       const fileType = await fileModel.getTypeById(fileId); // 1 = ppm, 2 = ppb
       const unit = fileType === 1 ? "ppm" : "ppb";
 
@@ -43,6 +44,7 @@ const miniTableService = {
 
       return miniTableData;
     } catch (err) {
+  
       console.error("miniTableService error:", err);
       throw err;
     }
@@ -50,19 +52,24 @@ const miniTableService = {
 
   async getSJSMiniTableForElement(fileId, solutionLabel, element) {
   try {
+    console.log("Fetching SJS mini table for element:", element, "in fileId:", fileId, "with solutionLabel:", solutionLabel);
     const fileType = await fileModel.getTypeById(fileId); // 1 = ppm, 2 = ppb
+    element += "_Corrected";
 
     const rows = await TableModel.getMiniTableRaw(fileId, solutionLabel, element);
     if (!rows || rows.length === 0) return [];
 
-    const sjsData = await TableModel.getSJSRows([element]);
-    const sjsStd = sjsData?.[0]?.[element];
-    const sjsError = sjsData?.[1]?.[element];
+    const [sjsStdRow, errorRow] = await TableModel.getSJSRows([element]);
+    const sjsStd = parseFloat(sjsStdRow[element]);
+    
+    const sjsError = 10;
+    console.log("SJS Std for element:", sjsStdRow);
 
     const errorFactor = isNaN(sjsStd) ? 1 : sjsStd;
     const errorTolerance = isNaN(sjsStd) || isNaN(sjsError) || sjsStd === 0
   ? 0
-  : parseFloat(((sjsError / sjsStd) * 100).toFixed(2));
+  : 10;
+  console.log("Error factor:", errorFactor, "Error tolerance:", errorTolerance);
 
 
     const miniTableData = rows
